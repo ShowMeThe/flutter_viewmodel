95100fe74b87e1f71db321be2b07d7649c6500a8
ken <c.uvsuv@163.com>
2020-11-28 16:58:50 +0800

    update

--- lib/base/vm/ViewModelStore.dart
+++ lib/base/vm/ViewModelStore.dart
@@ -3,14 +3,17 @@ import 'package:flutter_viewmodel/base/vm/ViewModel.dart';
 class ViewModelStore {
   Map<String, ViewModel> _map = Map();
 
-  void put<T extends ViewModel>(String key,  T viewModel) {
+  void put<T extends ViewModel>(String key, T viewModel) {
+    ViewModel oldViewModel = _map[key];
+    if (oldViewModel != null) {
+      oldViewModel.onCleared();
+    }
     _map[key] = viewModel;
   }
 
   T get<T extends ViewModel>(String key) {
     return _map[key];
   }
-
 
   void clear() {
     for (ViewModel vm in _map.values) {
